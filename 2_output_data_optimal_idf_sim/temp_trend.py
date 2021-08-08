7845e422c7f127c2cc4b0e4fd02a276cdc334674
lau175 <56656231+lau175@users.noreply.github.com>
2021-08-08 13:27:04 +0200

    Add files via upload

--- 2_output_data_optimal_idf_sim/temp_trend.py
+++ 2_output_data_optimal_idf_sim/temp_trend.py
@@ -3,7 +3,7 @@ import csv
 import matplotlib.pyplot as plt
 import numpy as np
 
-df=pd.read_csv('data_2.csv')
+df=pd.read_csv('data_double_off_nwschd_3110.csv')
 
 df.plot(x='Date/Time', y='avg_T_in')
 plt.xticks(rotation=45)
